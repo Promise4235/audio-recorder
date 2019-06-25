e37264856762743e72ddbf01255c0138c828d444
satish-chimple <36834824+satish754ss@users.noreply.github.com>
2019-06-25 18:03:55 +0530

    Updated. title name and audio name.

--- lib/create_recording.dart
+++ lib/create_recording.dart
@@ -30,7 +30,7 @@ class _RecordingPageState extends State<RecordingPage>
   Firestore firestore = Firestore();
   Stream<QuerySnapshot> dataStream, savedStreamData;
   StorageUploadTask task;
-  String userId, loadingStatus = 'Please wait...', _text;
+  String userId, loadingStatus = 'Please wait...', _text,_title='';
   PageController controller = PageController();
   bool isRecorded = false, isSent = true, isLoading = false;
   int flag = 0, count = 0;
@@ -87,19 +87,21 @@ class _RecordingPageState extends State<RecordingPage>
 
     dataStream =
         firestore.collection('data').snapshots().timeout(Duration(seconds: 2));
+    firestore.settings(timestampsInSnapshotsEnabled: true);
     dataStream.forEach((data) {
       for (int i = 0; i < data.documents.length; i++) {
         documentSnapshot = data.documents[i];
-        if (documentSnapshot['status'] != 'translated') {
-          print('done');
+        var doc=documentSnapshot['status'];
+        if (documentSnapshot['status'] != 'translated' ||doc ==null) {
           _text = documentSnapshot['text'];
+          _title=documentSnapshot['title'];
           setState(() {
             dataStatus = DataStatus.loaded;
           });
           break;
         } else {
           count++;
-          if (count == data.documents.length) {
+          if (count == data.documents.length - 1) {
             setState(() {
               dataStatus = DataStatus.noTranslation;
             });
@@ -120,7 +122,7 @@ class _RecordingPageState extends State<RecordingPage>
       isSent = false;
     });
     final StorageReference firebaseStorageRef =
-        FirebaseStorage.instance.ref().child('${Uuid().v1()}.m4a');
+        FirebaseStorage.instance.ref().child('${_title}.m4a');
     String audioUrl = await firebaseStorageRef.getPath();
     task = firebaseStorageRef.putFile(
       File(recorder.filePath),
@@ -130,7 +132,7 @@ class _RecordingPageState extends State<RecordingPage>
         .collection('data')
         .document(documentSnapshot.documentID)
         .setData({
-      'title': documentSnapshot['title'],
+      'title': _title+ '.m4a',
       'audio_url': audioUrl,
       'user_id': userId,
       'status': 'translated',
@@ -139,17 +141,23 @@ class _RecordingPageState extends State<RecordingPage>
       setState(() {
         loadingStatus = 'sent';
       });
-      new Future.delayed(Duration(seconds: 1), () {
-        setState(() {
-          dataStatus = DataStatus.loaded;
-        });
+      setState(() {
+        dataStatus = DataStatus.loaded;
+        isSent = true;
+        icons = Icon(
+          Icons.mic,
+          color: Colors.white,
+          size: 35.0,
+        );
+        textIcon = 'Record';
+        status = Status.record;
       });
     });
   }
 
   @override
   didUpdateWidget(RecordingPage oldWidget) {
-    print('didUpdateWidget');
+    print('didUpdateWidget'); 
     super.didUpdateWidget(oldWidget);
   }
 
@@ -179,7 +187,8 @@ class _RecordingPageState extends State<RecordingPage>
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
-        title: Text('Record'),
+        title: Text(_title),
+        centerTitle: true,
       ),
       body: _buildUI(),
     );
